8770a8f870feaa8552cd53b0c3abfaa1874901a3
Abhra Dey <abhradey@Abhras-MacBook-Pro.local>
2018-11-10 20:26:19 +0530

    Initial api connections and project layout for BooksApp

---
+++ app_api/BooksApp/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/abhradey/Documents/ProjectWork/BooksApp/BooksApp/app_api/BooksApp/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
