9372bd1415c2f9f2948cb55f29eaaf477033a017
Abhra Dey <abhradey@Abhras-MacBook-Pro.local>
2018-11-16 12:13:46 +0530

    Creating the books db

--- app_api/BooksAppProject/BooksApp/models.py
+++ app_api/BooksAppProject/BooksApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    cache_id = models.CharField(max_length=20)
+    title = models.CharField(max_length=50)
+    kind = models.CharField(max_length=25)
+    snippet = models.TextField()
+    display_link = models.CharField(max_length=50)
+    link = models.TextField()
+    image_link = models.TextField()
+    thumbnail_link = models.TextField()
+    formatted_url = models.TextField()
+
