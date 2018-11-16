04f9532d0572f57a858ee40d4cada685d9764bf0
Abhra Dey <abhradey@Abhras-MacBook-Pro.local>
2018-11-16 20:11:27 +0530

    Storing all the searched results in database

--- app_api/BooksAppProject/BooksApp/controllers/search_controller.py
+++ app_api/BooksAppProject/BooksApp/controllers/search_controller.py
@@ -1,5 +1,6 @@
 
 from ..common import utils
+from ..models import Books
 import requests, json
 
 def fetch_search_data(request):
@@ -11,7 +12,25 @@ def fetch_search_data(request):
     }
     response = requests.get(url, headers=headers)
     search_results = json.loads(response.content.decode('utf-8'))
+    update_books_db(search_results["items"])
     return search_results["items"]
+
+# The below method (update_books_db) is temporary and should be replaced by an async call to update the db 
+#     with searched results
+def update_books_db(results):
+    for book_info in results:
+        book = Books(
+            cache_id=book_info["cacheId"],
+            title=book_info["title"],
+            kind=book_info["kind"],
+            snippet=book_info["snippet"],
+            display_link=book_info["displayLink"],
+            link=book_info["link"],
+            image_link=book_info["pagemap"]["cse_image"][0]["src"],
+            thumbnail_link=book_info["pagemap"]["cse_thumbnail"][0]["src"],
+            formatted_url=book_info["formattedUrl"]
+        )
+        book.save()
 
 def fetch_search_prefix(request):
     query_params = request.query_params
--- app_api/BooksAppProject/BooksApp/models.py
+++ app_api/BooksAppProject/BooksApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Books(models.Model):
     cache_id = models.CharField(max_length=20)
-    title = models.CharField(max_length=50)
+    title = models.TextField()
     kind = models.CharField(max_length=25)
     snippet = models.TextField()
     display_link = models.CharField(max_length=50)
